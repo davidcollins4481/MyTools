74cbf1a46d68f833c446fe69c896ceabed862a59
David Collins <davidcollins4481@gmail.com>
2012-06-01 22:18:05 -0400

    use bright colors add user name

--- bin/battery.sh
+++ bin/battery.sh
@@ -3,11 +3,11 @@
 percentage=`acpi | awk -F", " '{print $2 }' | sed 's|%||g'`
 
 if [ $percentage -ge 80 ]; then
-    color="\005{g}"
+    color="\005{G}"
 elif [ $percentage -lt 80 ] && [ $percentage -ge 20 ]; then
-    color="\005{y}"
+    color="\005{Y}"
 else
-    color="\005{r}"
+    color="\005{R}"
 fi
 
 echo -e $color "${percentage}%"
