900a4a2f81298070a9d7c0396f38ecb8a2769fcd
David Collins <davidcollins4481@gmail.com>
2013-02-22 22:33:05 -0500

    - add .gitignore - sms notify script

---
+++ bin/sms_notify.pl
@@ -0,0 +1,35 @@
+#use warnings;
+use strict;
+use Google::Voice;
+use YAML qw(LoadFile);
+use Getopt::Long;
+
+# Google::Voice is spitting out all kinds of stuff...don't want to see it
+BEGIN { $SIG{'__WARN__'} = sub { return; } }
+
+my $to_phone;
+my $to_msg;
+
+GetOptions (
+    "to=s" => \$to_phone, # numeric
+    "message=s"   => \$to_msg,
+);
+
+usage() if !$to_phone || !$to_msg;
+
+my $config = LoadFile('bin/config.yaml');
+my $gv;
+
+$gv = Google::Voice->new->login(
+    $$config{google_username},
+    $$config{google_password}
+);
+
+
+# Send sms
+$gv->send_sms($to_phone => $to_msg);
+
+sub usage {
+    print qq{Usage: sms_notify.pl -t xxxyyyzzzz -m "sms message text"\n};
+    exit 1;
+}
