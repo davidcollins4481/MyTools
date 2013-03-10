99e73868cd3f2005bfb9e8fa6d3c85f6628022f6
David Collins <davidcollins4481@gmail.com>
2013-03-10 14:45:00 +0000

    fix ip script...hopefully for good

--- bin/ip.sh
+++ bin/ip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # this is ugly!
-echo `/sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | grep -v 127`
+echo `/sbin/ifconfig  | grep -C 2 RUNNING | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | grep -v 127`
